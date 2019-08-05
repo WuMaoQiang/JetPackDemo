89ede2b0a97af58310637b3f12fae1f8a7707ede
Wilfried <p_maoqwu@tencent.com>
2019-08-05 10:18:58 +0800

    添加标识buildConfig

--- app/src/main/java/com/jetpack/MainActivity.java
+++ app/src/main/java/com/jetpack/MainActivity.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-
+        Log.i("xiaoqiang", "onCreate: " + BuildConfig.VENDER);
         userViewModel = UserViewModel.getInstance();
         userMutableLiveData = userViewModel.getUserMutableLiveData();
 
